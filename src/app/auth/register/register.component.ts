e7733aaffd25561ed266844d06dd42e3c1bb5a15
tmtafel <tmtafel@gmail.com>
2020-01-28 16:34:40 -0500

    updating register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,16 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-
-import { AuthService } from '../auth.service';
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+import { FormBuilder, FormGroup, Validators, FormControl, FormGroupDirective, NgForm, AbstractControl } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-register',
@@ -18,32 +8,48 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  isLinear = true;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
-  fourthFormGroup: FormGroup;
-  matcher = new MyErrorStateMatcher();
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  constructor(private formBuilder: FormBuilder) {}
+  formGroup: FormGroup;
+  nameFormGroup: FormGroup;
+  emailFormGroup: FormGroup;
 
+  // steps = [
+  //   { label: 'Confirm your name', content: 'Last name, First name.' },
+  //   { label: 'Confirm your contact information', content: '123-456-7890' },
+  //   { label: 'Confirm your address', content: '1600 Amphitheater Pkwy MTV' },
+  //   { label: 'You are now done', content: 'Finished!' }
+  // ];
+
+  /** Returns a FormArray with the name 'formArray'. */
+  get formArray(): AbstractControl | null { return this.formGroup.get('formArray'); }
+  constructor(private formBuilder: FormBuilder) { }
   ngOnInit() {
-    this.firstFormGroup = this.formBuilder.group({
-      firstCtrl: ['', [Validators.required, Validators.email]]
+    this.formGroup = this.formBuilder.group({
+      formArray: this.formBuilder.array([
+        this.formBuilder.group({
+          firstNameFormCtrl: ['', Validators.required],
+          lastNameFormCtrl: ['', Validators.required],
+        }),
+        this.formBuilder.group({
+          emailFormCtrl: ['', [Validators.required, Validators.email]]
+        }),
+      ])
     });
-    this.secondFormGroup = this.formBuilder.group({
-      secondCtrl: ['', Validators.required]
+
+    this.nameFormGroup = this.formBuilder.group({
+      firstNameCtrl: ['', Validators.required],
+      lastNameCtrl: ['', Validators.required],
     });
-    this.thirdFormGroup = this.formBuilder.group({
-      thirdCtrl: ['', Validators.required]
-    });
-    this.fourthFormGroup = this.formBuilder.group({
-      fourthCtrl: ['', Validators.required]
+
+    this.emailFormGroup = this.formBuilder.group({
+      emailCtrl: ['', Validators.email]
     });
   }
+
+  completeRegistration() {
+    const firstName = this.nameFormGroup.value.firstNameCtrl;
+    console.log(firstName);
+  }
+
   // registerForm: FormGroup;
   // errorMessage = '';
   // successMessage = '';
